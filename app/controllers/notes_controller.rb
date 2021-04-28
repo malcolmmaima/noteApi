bfc769baec26181bcc16ab18a855536d46fe2b2f
malcolmmaima <malcolmmaima@gmail.com>
2021-04-28 14:57:45 +0300

    Implement Index and Create Endpoints

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,15 @@
-class NotesController < ApplicationController
+class NotesController < ApplicationController 
+    def index
+        notes = Note.all    
+        render json: notes, status: :ok  
+    end
+
+    def create    
+        note = Note.create!(note_params)    
+        render json: note, status: :ok  
+    end​  
+
+    def note_params    
+        params.require(:note).permit(:text) 
+    end
 end
